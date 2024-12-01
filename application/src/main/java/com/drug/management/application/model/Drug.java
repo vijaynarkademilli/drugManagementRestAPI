ff8fe39d840786dbcac6100baec72441e3627373
N Vijay Kumar <vijaykumar.narkademilli@gmail.com>
2024-12-01 23:27:29 +0530

    Model Update

--- application/src/main/java/com/drug/management/application/model/Drug.java
+++ application/src/main/java/com/drug/management/application/model/Drug.java
@@ -24,17 +24,27 @@ public class Drug {
     @NotBlank(message = "Batch Number is required")
     private String batchNumber;
 
-    @NotBlank(message = "Manufacturer is required")
-    private String manufacturer;
+    @NotNull(message = "Expiry date is required")
+    private LocalDate expiryDate;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 0, message = "Stock must be greater than or equal to 0")
+    private Integer quantity;
 
     @NotNull(message = "Price is required")
     @Min(value = 0, message = "Price must be greater than or equal to 0")
     private Double price;
 
-    @NotNull(message = "Expiry date is required")
-    private LocalDate expiryDate;
+    @NotNull(message = "Amount is required")
+    @Min(value = 0, message = "Amount must be greater than or equal to 0")
+    private Double amount;
 
-    @NotNull(message = "Stock is required")
-    @Min(value = 0, message = "Stock must be greater than or equal to 0")
-    private Integer stock;
+    @NotNull(message = "MRP is required")
+    @Min(value = 0, message = "MRP must be greater than or equal to 0")
+    private Double mrp;
+
+    @NotNull(message = "GST is required")
+    @Min(value = 0, message = "MRP must be greater than or equal to 0")
+    private Double gst;
+
 }
